40f7c367f2b5f762781244b23984e1a3ef0e6bf6
kumardinesh98 <devadineshkumar98@gmail.com>
2021-11-22 22:46:03 -0800

    third time modified for checking

--- Employecase1.java
+++ Employecase1.java
@@ -1,4 +1,6 @@
 public class Employecase1 {
+	innt 
+		float
 	public static void main (String[]  args) {
 		int is_present = 1;
 		int per_day_wage=20;
@@ -7,6 +9,14 @@ public class Employecase1 {
 		double attendance = Math.floor((Math.random() *10) % 2);
 		System.out.println(attendance);
 		
+
+hello world
+git process
+main branch
+branch
+
+
+
 		if  (attendance == 1) {
 			System.out.println("employe iss present");
 			total_wage= working_hours*per_day_wage;
